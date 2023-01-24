2d728c85531c4dd926469112d4fd908b5a10b562
Vincent Casey <vinmcasey@gmail.com>
2023-01-24 09:13:25 -0600

    Update

--- testing/image_object_detection.py
+++ testing/image_object_detection.py
@@ -1,14 +1,15 @@
 import cv2
-from imread_from_url import imread_from_url
+# from imread_from_url import imread_from_url
 from yolov8 import YOLOv8
 
 #Initialize yolov8
-model_path = "models/best.onnx"
+model_path = "models/best18.onnx"
 yolov8_detector = YOLOv8(model_path, conf_thres=0.35, iou_thres=0.3)
 
 #Read image
-img_url = "https://media.newyorker.com/photos/5e5ed01f39e0e500082b73b6/master/w_2560%2Cc_limit/Rosner-CoronavirusPanicShopping.jpg"
-img = imread_from_url(img_url)
+# img_url = "https://media.newyorker.com/photos/5e5ed01f39e0e500082b73b6/master/w_2560%2Cc_limit/Rosner-CoronavirusPanicShopping.jpg"
+# img = imread_from_url(img_url)
+img = cv2.imread("testimg.jpg")
 
 #Detect Objects
 boxes, scores, class_ids = yolov8_detector(img)
